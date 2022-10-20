ad61dfe1e6d0253ddb197a27cd79c2b358665819
Emmanuel Adeyeye <emmanueloluwagbemi99@gmail.com>
2022-10-20 09:38:37 +0100

    update

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,40 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
 
 /**
-  * main - ...
-  * @argc: ...
-  * @argv: ...
-  *
-  * Return: ...
-  */
+ * main - print opcodes of `main' function up to specified number of bytes
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ *
+ * Return: EXIT_SUCCESS, or 1 if failure
+ */
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
+	short nbytes, i;
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		puts("Error");
+		exit(1);
 	}
-
-	return (0);
+	nbytes = atoi(argv[1]);
+	if (nbytes < 0)
+	{
+		puts("Error");
+		exit(2);
+	}
+	if (nbytes == 0)
+	{
+		putchar('\n');
+		exit(EXIT_SUCCESS);
+	}
+	printf("%02x", *((unsigned char *) (main)));
+	for (i = 1; i < nbytes; ++i)
+		printf(" %02x", *((unsigned char *) (main + i)));
+	putchar('\n');
+	exit(EXIT_SUCCESS);
 }
-
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+#ALX_SE 0x12 Singly linked lists
